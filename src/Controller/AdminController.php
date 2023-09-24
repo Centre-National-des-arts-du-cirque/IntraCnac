8d473344c3e439f14f46aef087dc97ca888eb898
Tom  Raineri <tomstory9@gmail.com>
2023-09-24 13:16:38 +0200

    feat : really basic css and html adminDashBoard

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\TicketRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+class AdminController extends AbstractController
+{
+    private $tokenStorage;   
+    
+    public function __construct(TokenStorageInterface $tokenStorage)
+    {
+        $this->tokenStorage = $tokenStorage;
+    }
+    #[Route('/admin', name: 'app_admin')]
+    public function index(TicketRepository $tr): Response
+    {   
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        $tickets = $tr->findBy([],['date' => 'DESC']);
+        return $this->render('admin/index.html.twig', [
+            'tickets' => $tickets,
+        ]);
+    }
+}
