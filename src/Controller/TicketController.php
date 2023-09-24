6348aec91e2e4f842bafb593a6e0e7905943a4b5
Tom  Raineri <tomstory9@gmail.com>
2023-09-24 10:56:35 +0200

    Add denyAcces when not authenticated

--- src/Controller/TicketController.php
+++ src/Controller/TicketController.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Controller;
+
 use App\Entity\BuildingTicket;
 use App\Entity\ItTicket;
+use App\Entity\VehicleTicket;
 use App\Form\Ticket\BuildingTicketFormType;
 use App\Form\Ticket\ItTicketFormType;
 use Doctrine\ORM\EntityManagerInterface;
@@ -18,7 +20,7 @@ class TicketController extends AbstractController
     private $em;
     private $tokenStorage;
 
-    public function __construct( TokenStorageInterface $tokenStorage, EntityManagerInterface $em)
+    public function __construct(TokenStorageInterface $tokenStorage, EntityManagerInterface $em)
     {
         $this->em = $em;
         $this->tokenStorage = $tokenStorage;
@@ -27,46 +29,73 @@ class TicketController extends AbstractController
     #[Route('/ItTicket', name: 'app_ItTicket')]
     public function ittTicket(Request $request): Response
     {   
-       $user = $this->tokenStorage->getToken()->getUser();
+        $this->denyAccessUnlessGranted('ROLE_USER');
+        $user = $this->tokenStorage->getToken()->getUser();
 
-       $itTicket = new ItTicket();
-       $form = $this->createForm(ItTicketFormType::class, $itTicket);
-        
-       $form->handleRequest($request);
-       if ($form->isSubmitted() && $form->isValid()) {
+        $itTicket = new ItTicket();
+        $form = $this->createForm(ItTicketFormType::class, $itTicket);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
             $itTicket->setCreateBy($user);
             $itTicket->setSolved(false);
-            
-
 
             $this->em->persist($itTicket);
             $this->em->flush();
 
             return $this->redirectToRoute('app_login');
         }
+
         return $this->render('ticket/ItTicket/index.html.twig', [
             'ItTicketForm' => $form->createView(),
         ]);
     }
+
     #[Route('/BuildingTicket', name: 'app_BuildingTicket')]
     public function BuildingTicket(Request $request): Response
     {   
-       $user = $this->tokenStorage->getToken()->getUser();
+        $this->denyAccessUnlessGranted('ROLE_USER');
+        $user = $this->tokenStorage->getToken()->getUser();
 
-       $BuildingTicket = new BuildingTicket();
-       $form = $this->createForm(BuildingTicketFormType::class, $BuildingTicket);
-        
-       $form->handleRequest($request);
-       if ($form->isSubmitted() && $form->isValid()) {
+        $BuildingTicket = new BuildingTicket();
+        $form = $this->createForm(BuildingTicketFormType::class, $BuildingTicket);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
             $BuildingTicket->setCreateBy($user);
             $BuildingTicket->setSolved(false);
             $this->em->persist($BuildingTicket);
             $this->em->flush();
 
             return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('ticket/BuildingTicket/index.html.twig', [
+            'BuildingTicketForm' => $form->createView(),
+        ]);
     }
-    return $this->render('ticket/BuildingTicket/index.html.twig', [
-        'BuildingTicketForm' => $form->createView(),
-    ]);
-}
+
+    #[Route('/VehicleTicket', name: 'app_VehicleTicket')]
+    public function VehicleTicket(Request $request): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+        $user = $this->tokenStorage->getToken()->getUser();
+
+        $VehicleTicket = new VehicleTicket();
+        $form = $this->createForm(VehicleTicketFormType::class, $VehicleTicket);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $VehicleTicket->setCreateBy($user);
+            $VehicleTicket->setSolved(false);
+            $this->em->persist($VehicleTicket);
+            $this->em->flush();
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('ticket/VehicleTicket/index.html.twig', [
+            'VehicleTicketForm' => $form->createView(),
+        ]);
+    }
 }
