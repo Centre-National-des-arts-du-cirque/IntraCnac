ab8dfec4f919a82e3d15be021d740c8e42b1ebfd
Tom  Raineri <tomstory9@gmail.com>
2023-09-26 22:12:27 +0200

    Fix css and chart size

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Repository\BuildingTicketRepository;
+use App\Repository\ItTicketRepository;
 use App\Repository\TicketRepository;
+use App\Repository\VehicleTicketRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,49 +13,44 @@ use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInt
 use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
 use Symfony\UX\Chartjs\Model\Chart;
 
-
 class AdminController extends AbstractController
 {
-    private $tokenStorage;   
-    
+    private $tokenStorage;
+
     public function __construct(TokenStorageInterface $tokenStorage)
     {
         $this->tokenStorage = $tokenStorage;
     }
+
     #[Route('/admin', name: 'app_admin')]
     public function index(TicketRepository $tr): Response
-    {   
+    {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
 
-        $tickets = $tr->findBy([],['date' => 'DESC', 'solved' => 'ASC']);
+        $tickets = $tr->findBy([], ['date' => 'DESC', 'solved' => 'ASC']);
+
         return $this->render('admin/index.html.twig', [
             'tickets' => $tickets,
         ]);
     }
+
     #[Route('/admin/chart', name: 'app_admin_chart')]
-    public function chart(ChartBuilderInterface $chartBuilder):Response
-    {   
-        $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
+    public function chart(ChartBuilderInterface $chartBuilder, ItTicketRepository $itTicketRepositoryBuild , BuildingTicketRepository $buildingTicketRepository , VehicleTicketRepository $vehicleTicketRepository): Response
+    {
+        $chart = $chartBuilder->createChart(Chart::TYPE_PIE);
         $chart->setData([
-            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            'labels' => ['IT', 'Building', 'Vehicle'],
             'datasets' => [
                 [
                     'label' => 'My First dataset',
                     'backgroundColor' => 'rgb(255, 99, 132)',
                     'borderColor' => 'rgb(255, 99, 132)',
-                    'data' => [0, 10, 5, 2, 20, 30, 45],
+                    'data' => [$itTicketRepositoryBuild->count([]), $buildingTicketRepository->count([]), $vehicleTicketRepository->count([])],
                 ],
             ],
         ]);
 
-        $chart->setOptions([
-            'scales' => [
-                'y' => [
-                    'suggestedMin' => 0,
-                    'suggestedMax' => 100,
-                ],
-            ],
-        ]);
+       
 
         return $this->render('admin/chart.html.twig', [
             'chart' => $chart,
