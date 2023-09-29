dcf44c2f770090ddf9d7be508660da717eb7936b
Tom Raineri <assistance.technique@cnac.fr>
2023-09-29 13:53:15 +0200

    Feat: show route for user and Admin

--- src/Controller/TicketController.php
+++ src/Controller/TicketController.php
@@ -4,12 +4,14 @@ namespace App\Controller;
 
 use App\Entity\BuildingTicket;
 use App\Entity\ItTicket;
+use App\Entity\Ticket;
 use App\Entity\VehicleTicket;
 use App\Form\Ticket\BuildingTicketFormType;
 use App\Form\Ticket\ItTicketFormType;
 use App\Form\Ticket\VehicleTicketFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -99,6 +101,124 @@ class TicketController extends AbstractController
         return $this->render('ticket/VehicleTicket/index.html.twig', [
             'VehicleTicketForm' => $form->createView(),
             
+        ]);
+    }
+    #[Route('/show/{ticketId}', name: 'app_show' , requirements: ['ticket' => '\d+'])]
+    public function show(int $ticketId): JsonResponse
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+        if($this->tokenStorage->getToken()->getUser() != $this->em->find(Ticket::class,$ticketId)->getCreateBy()){
+            return new JsonResponse([
+                'error' => 'vous n\'avez pas les droits pour voir ce ticket'
+            ]);
+        }
+        $ticket = null;
+        if ($this->em->getRepository(ItTicket::class)->find($ticketId) ) {
+            $ticket = $this->em->getRepository(ItTicket::class)->find($ticketId);
+            return new JsonResponse([
+                'title' => $ticket->getTitle(),
+                'pcName' => $ticket->getPcName(),
+                'errorCode' => $ticket->getErrorCode(),
+                'localisation' => $ticket->getLocalisation(),
+                'description' => $ticket->getDescription(),
+                'date' => $ticket->getDate(),
+                'solved' => $ticket->isSolved(),
+                'createByName' => $ticket->getCreateBy()->getName(),
+                'createByFirstName' => $ticket->getCreateBy()->getLastName(),
+            ]);
+        }
+        if ($this->em->getRepository(BuildingTicket::class)->find($ticketId)) {
+            $ticket = $this->em->getRepository(BuildingTicket::class)->find($ticketId);
+            return new JsonResponse([
+                'title' => $ticket->getTitle(),
+                'localisation' => $ticket->getLocalisation(),
+                'description' => $ticket->getDescription(),
+                'date' => $ticket->getDate(),
+                'solved' => $ticket->isSolved(),
+                'site' => $ticket->getSite(),
+                'createByName' => $ticket->getCreateBy()->getName(),
+                'createByFirstName' => $ticket->getCreateBy()->getLastName(),
+            ]);
+        }
+        if ($this->em->getRepository(VehicleTicket::class)->find($ticketId)) {
+            $ticket = $this->em->getRepository(VehicleTicket::class)->find($ticketId);
+            return new JsonResponse([
+                'title' => $ticket->getTitle(),
+                'brand' => $ticket->getBrand(),
+                'immatriculation' => $ticket->getImmatriculation(),
+                'description' => $ticket->getDescription(),
+                'date' => $ticket->getDate(),
+                'solved' => $ticket->isSolved(),
+                'createByName' => $ticket->getCreateBy()->getName(),
+                'createByFirstName' => $ticket->getCreateBy()->getLastName(),
+            ]);
+        }
+        
+      
+    }
+    #[Route('/admin/show/{ticketId}', name: 'app_showAdmin' , requirements: ['ticket' => '\d+'])]
+    public function showAdmin(int $ticketId)
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        
+        $ticket = null;
+        if ($this->em->getRepository(ItTicket::class)->find($ticketId) ) {
+            $ticket = $this->em->getRepository(ItTicket::class)->find($ticketId);
+            return new JsonResponse([
+                'title' => $ticket->getTitle(),
+                'pcName' => $ticket->getPcName(),
+                'errorCode' => $ticket->getErrorCode(),
+                'localisation' => $ticket->getLocalisation(),
+                'description' => $ticket->getDescription(),
+                'date' => $ticket->getDate(),
+                'solved' => $ticket->isSolved(),
+                'createByName' => $ticket->getCreateBy()->getName(),
+                'createByFirstName' => $ticket->getCreateBy()->getLastName(),
+            ]);
+        }
+        if ($this->em->getRepository(BuildingTicket::class)->find($ticketId)) {
+            $ticket = $this->em->getRepository(BuildingTicket::class)->find($ticketId);
+            return new JsonResponse([
+                'title' => $ticket->getTitle(),
+                'localisation' => $ticket->getLocalisation(),
+                'description' => $ticket->getDescription(),
+                'date' => $ticket->getDate(),
+                'solved' => $ticket->isSolved(),
+                'site' => $ticket->getSite(),
+                'createByName' => $ticket->getCreateBy()->getName(),
+                'createByFirstName' => $ticket->getCreateBy()->getLastName(),
+            ]);
+        }
+        if ($this->em->getRepository(VehicleTicket::class)->find($ticketId)) {
+            $ticket = $this->em->getRepository(VehicleTicket::class)->find($ticketId);
+            return new JsonResponse([
+                'title' => $ticket->getTitle(),
+                'brand' => $ticket->getBrand(),
+                'immatriculation' => $ticket->getImmatriculation(),
+                'description' => $ticket->getDescription(),
+                'date' => $ticket->getDate(),
+                'solved' => $ticket->isSolved(),
+                'createByName' => $ticket->getCreateBy()->getName(),
+                'createByFirstName' => $ticket->getCreateBy()->getLastName(),
+            ]);
+    }
+}
+    #[Route('/solved/{ticket}', name: 'app_solved' , requirements: ['ticket' => '\d+'])]
+    public function solved(Ticket $ticket): JsonResponse
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+        $user = $this->tokenStorage->getToken()->getUser();
+
+        $ticket = $this->em->getRepository(Ticket::class)->find($ticket);
+
+        if ($ticket->getCreateBy() == $user) {
+            $ticket->setSolved(true);
+            $this->em->persist($ticket);
+            $this->em->flush();
+        }
+
+        return new JsonResponse([
+            'ticket' => $ticket,
         ]);
     }
 }
