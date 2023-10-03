5c9a67fa1d3d393a6481057dced017fcd2ea5f2f
Tom Raineri <assistance.technique@cnac.fr>
2023-10-03 10:55:32 +0200

    fix : routing and add UserController

---
+++ src/Controller/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+class UserController extends AbstractController
+{
+    private $tokenStorage;
+
+    public function __construct(TokenStorageInterface $tokenStorage)
+    {
+        $this->tokenStorage = $tokenStorage;
+    }
+    #[Route('/profil', name: 'app_user')]
+    public function index(): Response
+    {
+        
+    }
+}
