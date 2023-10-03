850fbb28e2c24e40a54facb2d58feeb83aa3cf53
Tom Raineri <assistance.technique@cnac.fr>
2023-10-03 11:14:54 +0200

    add profil controller for lambda user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TicketRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,8 +17,27 @@ class UserController extends AbstractController
         $this->tokenStorage = $tokenStorage;
     }
     #[Route('/profil', name: 'app_user')]
-    public function index(): Response
+    public function redirection(): Response
     {
-        
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        if ($this->isGranted('ROLE_ADMIN')) {
+            return $this->redirectToRoute('app_admin');
+        }
+        else {
+            return $this->redirectToRoute('app_user_profil');
+        }
     }
+    #[Route('/monprofil',name:'app_user_profil')]
+    public function index(TicketRepository $ticketRepository): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        $tickets = $ticketRepository->findBy(['createBy'=>$this->getUser()], ['date' => 'DESC', 'solved' => 'ASC']);
+        
+        return $this->render('user/index.html.twig',[
+            'tickets' => $tickets
+        ]);
+    }
+
 }
