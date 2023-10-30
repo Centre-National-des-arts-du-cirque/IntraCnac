9549fee3e426e550ea1292d4b7305a366c494464
Tom Raineri <assistance.technique@cnac.fr>
2023-10-30 16:56:01 +0100

    Add: read of the csv File

--- src/Controller/XMLCertificationController.php
+++ src/Controller/XMLCertificationController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Form\XMLCertificationType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class XMLCertificationController extends AbstractController
 {
-    #[Route('/x/m/l/certification', name: 'app_x_m_l_certification')]
-    public function index(): Response
+    #[Route('/xmlcertification', name: 'app_x_m_l_certification')]
+    public function index(Request $request): Response
     {
+
+        $form = $this->createForm(XMLCertificationType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $csvFile = $form->get('File')->getData();
+            $csv = new \ParseCsv\Csv();
+            $csv->auto($csvFile->getPathname());
+            dd($csv->data);
+        }
         return $this->render('xml_certification/index.html.twig', [
-            'controller_name' => 'XMLCertificationController',
+            'XMLCertification' => $form->createView(),
         ]);
     }
 }
